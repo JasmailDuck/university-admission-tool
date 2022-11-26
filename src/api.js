2f0c2601201f828328e0e57104a22c8360d022eb
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2022-11-26 15:53:10 -0700

    Dev program page (#49)
    * Added list of filters
    
    * Added the program cards and finished the first filter

--- src/api.js
+++ src/api.js
@@ -2,11 +2,10 @@ import axios from "axios";
 
 // an instance of the Axios driver. Used everywhere for API calls
 const instance = axios.create({
-  baseURL: "http://35.209.74.28:4000/api",
+  baseURL: "http://localhost:3003/api",
   headers: {
     "Content-Type": "application/json",
-  },
-  withCredentials: true
+  }
 });
 
 export default instance;
