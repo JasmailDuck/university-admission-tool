b396b5fe67192c72c65ab4cc54e7f450135b09af
Jasmail Singh Duck <jasmailduck@icloud.com>
2022-12-07 18:17:05 -0700

    PDF FILE VIEW

--- src/Pages/admin-userFileManagement/cmpnts/fileView.jsx
+++ src/Pages/admin-userFileManagement/cmpnts/fileView.jsx
@@ -3,7 +3,7 @@ import FILE_SERVICE from '../../../services/file_service';
 
 import { Document, Page,  pdfjs } from 'react-pdf/dist/esm/entry.webpack5'
 
-//pdfjs.GlobalWorkerOptions.workerSrc = `cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
 
 class fileView extends Component {
 
@@ -11,14 +11,18 @@ class fileView extends Component {
     constructor(props) {
   
       super(props);
+      this.nextPage = this.nextPage.bind(this)
+      this.prevPage =  this.prevPage.bind(this)
+      
+      //this.setPageNum = this.setPageNum.bind(this)
   
-  
-  
+      sessionStorage.setItem("pageNum", 1)
       this.state = {
         id: sessionStorage.getItem("fileID"),
         file: "",
         pageNum: 1,
-        test: false
+        test: false,
+        fileObject: "",
       };
     }
   
@@ -30,11 +34,7 @@ class fileView extends Component {
         file: file,
       });
     }
-    setPageNum(num) {
-        this.setState({
-          pageNum: num,
-        });
-      }
+    
 
       setTest(test) {
         this.setState({
@@ -43,38 +43,89 @@ class fileView extends Component {
       }
     
   
+      setFileObject(file) {
+        this.setState({
+          fileObject: file,
+        });
+      }
 
+      setPageNum(num) {
+        this.setState({
+          pageNum: num,
+        });
+      }
     
   
     getFileInformation() {
       FILE_SERVICE.getFileByID(this.state.id).then((file)=> {
         this.setFile(file.file)
-        console.log(file.file);       
+        this.setFileObject(file)
+        
       })
     }
   
     
-  
+    
+
+    nextPage() {
+      this.setPageNum(this.state.pageNum + 1)
+    }
+
+    prevPage() {
+      this.setPageNum(this.state.pageNum - 1)
+    }
   
   
     componentDidMount() {
       this.getFileInformation();
       
-  
     }
     
   
     render() {
+
         //let i = this.state.pageNum
-        
+        // parseInt(sessionStorage.getItem("pageNum"))
         
         //const url = URL.createObjectURL()
         return(
-            <div className='flex'>
-                <Document file={this.state.file} options>
-                    <Page pageNumber={this.state.pageNum} />
-                </Document>
-                
+            <div className='flex h-full'>
+
+                <div id='pdf-view' className=' flex-1 flex justify-center  bg-neutral-100 p-2'>
+                  <Document file={this.state.file} className=" "  >
+                    <Page pageNumber={this.state.pageNum} renderTextLayer= {false} />
+                  </Document>
+                </div>
+
+                <div className=' flex flex-col  flex-1'>
+                <div id='fileController' className='mb-8 p-2 text-2xl'>
+                    File Details
+                  </div>
+                  
+                  <div id='pageController' className='p-4 flex flex-col justify-center items-center border-y'>
+                    <div className='mb-5'>
+                      Page View Controller
+                    </div>
+                    <div className='flex'>
+                      <button onClick={this.prevPage}  className='p-2 bg-neutral-200 rounded-2xl mx-5'>{"< Previous Page "}</button>
+                      <span className='p-2  mx-5'>{this.state.pageNum}</span>
+                      <button onClick={this.nextPage} className='p-2 bg-neutral-200 rounded-2xl mx-5'>{"Next Page >"}</button>
+                    </div>
+                  </div>
+
+                  <div className='p-4'>
+                    File Uploaded By: {this.state.fileObject.email}
+                  </div>
+
+                  <div className='p-4'>
+                    File Has Been Reviewed: {this.state.fileObject.reviewed}
+                  </div>
+
+                  <div className='p-4'>
+                    Comments: {this.state.fileObject.comments}
+                  </div>                   
+                </div>
+              
             </div>
             
         )
